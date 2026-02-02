d221b8d5d44abf4067185339ef8d2b047b44a33f
uzma-azeezy <uzmaazeezz12@gmail.com>
2026-02-02 17:28:00 +0530

    Primitive Datatypes

---
+++ Primitives.java
@@ -0,0 +1,19 @@
+public class Primitives {
+    public static void main(String[] args) {
+
+        int rollno = 169;
+        char letter = 'u';
+        float marks = 9.2f;
+        double largeDecimalNumbers = 986.73683628;
+        long largeIntNumbers = 9008402934L;
+        boolean check = true;
+
+        System.out.println(rollno);
+        System.out.println(letter);
+        System.out.println(marks);
+        System.out.println(largeDecimalNumbers);
+        System.out.println(largeIntNumbers);
+        System.out.println(check);
+    }
+}
+
