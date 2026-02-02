05f53d4bb82345c3219faba65ed53d29f3cd1ebf
uzma-azeezy <uzmaazeezz12@gmail.com>
2026-02-02 17:41:38 +0530

    Input-demo

---
+++ Input.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Input{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String name = input.nextLine();
+        System.out.println("My name is "+ name);
+    }
+}
